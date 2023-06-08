664affd0590d96867717a0cae002e33d138d4eeb
AirplaneGoBrr <mottlcompound@gmail.com>
2023-06-08 01:47:38 -0700

    I suck at this lol

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = 8080
 //Express setup
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use('/files', express.static('files'), serveIndex('files', {'icons': true}))
 
 app.get('/', (req, res) => {
     res.render("index")
