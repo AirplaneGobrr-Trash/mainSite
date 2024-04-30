efbeea0d09d1125e19b7835e30deb1034b3d3510
AirplaneGoBrr <mottlcompound@gmail.com>
2024-04-30 09:50:44 -0700

    Add arc

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Imports/Setup
+const axios = require('axios').default;
 const express = require('express');
 const app = express();
 
@@ -8,6 +9,17 @@ const port = 8080
 //Express setup
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+// https://arc.io/arc-sw.js
+app.get("/arc-sw.js", (req, res) => {
+    axios.get("https://arc.io/arc-sw.js").then((response) => {
+        res.contentType(response.headers['content-type'])
+        res.send(response.data)
+    }).catch((e) => {
+        console.log(e)
+        res.send("Error")
+    })
+})
 
 app.get('/', (req, res) => {
     res.render("index")
@@ -25,12 +37,12 @@ app.get('/about', (req, res) => {
     res.render("about")
 })
 
-app.get("/ip", (req, res)=>{
-    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+app.get("/ip", (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     res.type('text/plain').send(ip)
 })
 
-app.get("/server", (req, res)=>{
+app.get("/server", (req, res) => {
     res.redirect("minecraft://?addExternalServer=Robot%20Server|airplanegobrr.us.to:25565")
 })
 
